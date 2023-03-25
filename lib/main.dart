24adee89d1cdd6149fc8c8e77c95c7729de3f26a
miakh <bujnmi@gmail.com>
2023-03-25 20:25:59 +0100

    colors, fonts, buttons

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: 'RussoOne',
+        secondaryHeaderColor: const Color(0xFFBA5D3F), colorScheme: ColorScheme.fromSwatch(primarySwatch: primarySwatch).copyWith(background: backgroundColor)
       ),
       home: const MyHomePage(title: 'Absolventský Velehrad'),
     );
@@ -78,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Row(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -95,21 +96,116 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Plus plus plus!:',
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                MainPageButton(
+                  onPressed: _programmPressed,
+                  backgroundColor: primaryBlue1,
+                  child: const Icon(Icons.calendar_month),
+                ),// <-- Icon
+                const Text("Program"), // <-- Text
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                MainPageButton(
+                  onPressed: _programmPressed,
+                  backgroundColor: primaryYellow,
+                  child: const Icon(Icons.newspaper),
+                ),// <-- Icon
+                const Text("Ohlášky"), // <-- Text
+              ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                MainPageButton(
+                  onPressed: _programmPressed,
+                  backgroundColor: primaryRed,
+                  child: const Icon(Icons.map),
+                ),// <-- Icon
+                const Text("Mapa"), // <-- Text
+              ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                MainPageButton(
+                  onPressed: _programmPressed,
+                  backgroundColor: primaryBlue2,
+                  child: const Icon(Icons.info),
+                ),// <-- Icon
+                const Text("Info"), // <-- Text
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _programmPressed() {
+    ;
+  }
+
+
+}
+
+MaterialColor primarySwatch = const MaterialColor(
+  0xFF2C677B,
+  <int, Color>{
+    50: Color(0xFFE1F0F4),
+    100: Color(0xFFB4D9E4),
+    200: Color(0xFF84BFD3),
+    300: Color(0xFF55A5C2),
+    400: Color(0xFF3994B6),
+    500: Color(0xFF1D838A),
+    600: Color(0xFF176E6F),
+    700: Color(0xFF125954),
+    800: Color(0xFF0C4239),
+    900: Color(0xFF062E1E),
+  },
+);
+
+const backgroundColor = Color(0xFFD3D3D3);
+const primaryBlue1 = Color(0xFF2C677B);
+const Color primaryRed = Color(0xFFBA5D3F);
+const primaryYellow = Color(0xFFE0B73B);
+const primaryBlue2 = Color(0xFF2A77A0);
+
+ButtonStyle mainPageButtonStyle = OutlinedButton.styleFrom(
+    padding: const EdgeInsets.all(16),
+    tapTargetSize: MaterialTapTargetSize.padded,
+    backgroundColor: primaryRed,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8))
+);
+
+class MainPageButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+  final EdgeInsets margin;
+  final Color backgroundColor;
+
+  const MainPageButton({
+    Key? key,
+    required this.onPressed,
+    required this.child,
+    this.backgroundColor = primaryRed,
+    this.margin = const EdgeInsets.symmetric(horizontal: 8.0),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: margin,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: mainPageButtonStyle.copyWith(backgroundColor: MaterialStateProperty.all(backgroundColor)),
+        child: child,
+      ),
     );
   }
 }
