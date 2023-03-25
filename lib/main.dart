3de1e3fc84f22fe2a9e5f46b7e24269dbcf0110d
miakh <bujnmi@gmail.com>
2023-03-25 21:10:23 +0100

    added logo to mainPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -71,77 +72,85 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Column(
+
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Row(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                MainPageButton(
-                  onPressed: _programmPressed,
-                  backgroundColor: primaryBlue1,
-                  child: const Icon(Icons.calendar_month),
-                ),// <-- Icon
-                const Text("Program"), // <-- Text
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                MainPageButton(
-                  onPressed: _programmPressed,
-                  backgroundColor: primaryYellow,
-                  child: const Icon(Icons.newspaper),
-                ),// <-- Icon
-                const Text("Ohlášky"), // <-- Text
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                MainPageButton(
-                  onPressed: _programmPressed,
-                  backgroundColor: primaryRed,
-                  child: const Icon(Icons.map),
-                ),// <-- Icon
-                const Text("Mapa"), // <-- Text
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                MainPageButton(
-                  onPressed: _programmPressed,
-                  backgroundColor: primaryBlue2,
-                  child: const Icon(Icons.info),
-                ),// <-- Icon
-                const Text("Info"), // <-- Text
-              ],
-            ),
-          ],
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget> [
+          Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: SvgPicture.asset(
+            width: 250,
+            semanticsLabel: 'Absolventský Velehrad',
+            'images/avlogo.svg',
+          )),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 48.0),
+            child: Row(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  MainPageButton(
+                    onPressed: _programmPressed,
+                    backgroundColor: primaryBlue1,
+                    child: const Icon(Icons.calendar_month),
+                  ),// <-- Icon
+                  const Text("Program"), // <-- Text
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  MainPageButton(
+                    onPressed: _programmPressed,
+                    backgroundColor: primaryYellow,
+                    child: const Icon(Icons.newspaper),
+                  ),// <-- Icon
+                  const Text("Ohlášky"), // <-- Text
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  MainPageButton(
+                    onPressed: _programmPressed,
+                    backgroundColor: primaryRed,
+                    child: const Icon(Icons.map),
+                  ),// <-- Icon
+                  const Text("Mapa"), // <-- Text
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  MainPageButton(
+                    onPressed: _programmPressed,
+                    backgroundColor: primaryBlue2,
+                    child: const Icon(Icons.info),
+                  ),// <-- Icon
+                  const Text("Info"), // <-- Text
+                ],
+              ),
+            ],
         ),
+          )],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
