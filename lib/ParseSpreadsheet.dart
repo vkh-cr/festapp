921e250a933d8ba923348f7618a0463729edeea9
Dominik Macháček <gldkslfmsd@gmail.com>
2023-05-01 14:46:13 +0200

    storing changes in the app for parse-sheet
    This branch can be left inactive, just storing my edits. Could be useful.

--- lib/ParseSpreadsheet.dart
+++ lib/ParseSpreadsheet.dart
@@ -3,20 +3,46 @@ import 'dart:async';
 // import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:av_app/auth_helper.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ParseRegistrationsSheet {
   void parse() async {
     Map<String, String>? h = await AuthHelper.authenticate();
-    var v = await http.get(
-      Uri.parse(
-        // this should work, but it doesn't :(
-        "https://script.google.com/macros/s/AKfycbz8gUht2wTEckxY6CFKEI38YbxArqDC2xVL0dPQXhY/dev",
-        // ...but it works with this -- weird!
-        //"https://jsonplaceholder.typicode.com/albums/1",
-      ),
-      headers: h,
-    );
+    try {
+      var u = Uri.https("script.google.com",
+          "/macros/s/AKfycbz8gUht2wTEckxY6CFKEI38YbxArqDC2xVL0dPQXhY/dev");
+      print("tu");
+      var v = await http.get(
+        u,
+        // Uri.parse(
+        //   // this should work, but it doesn't :(
+        //   "https://script.google.com/macros/s/AKfycbz8gUht2wTEckxY6CFKEI38YbxArqDC2xVL0dPQXhY/dev",
+        //   // ...but it works with this -- weird!
+        //   //"https://jsonplaceholder.typicode.com/albums/1",
+        // ),
+        headers: h,
+      );
+      // var u = Uri.parse(
+      //   //     // this should work, but it doesn't :(
+      //   "https://script.google.com/macros/s/AKfycbz8gUht2wTEckxY6CFKEI38YbxArqDC2xVL0dPQXhY/dev",
+      //   //     // ...but it works with this -- weird!
+      //   //"https://jsonplaceholder.typicode.com/albums/1",
+      // );
+      // http.Request r = http.Request("get", u);
+      // r.maxRedirects = 5;
+      // r.followRedirects = true;
+      // try {await supabase.from("information").insert(data2);
+      //   var x = await r.send();
+      print("Tady");
+      print(v);
+    } catch (e) {
+      print(e);
+    }
+    return;
+  }
 
+  void insert(var supabase) async {
+    var x = await supabase.from("information");
     return;
   }
 
