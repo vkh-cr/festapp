a6d6943f03f98ecb157173774fe7a2ae9de900ce
miakh <2659269+miakh@users.noreply.github.com>
2023-06-18 13:59:22 +0200

    program timeline refact

---
+++ lib/widgets/ProgramTimeline.dart
@@ -0,0 +1,73 @@
+  import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:timelines/timelines.dart';
+
+import '../models/EventModel.dart';
+import '../styles/Styles.dart';
+
+  class ProgramTimeline extends StatefulWidget {
+    Function(int)? onEventPressed;
+
+    List<EventModel> events = [];
+
+   ProgramTimeline({super.key, required this.events, this.onEventPressed});
+
+    @override
+    _ProgramTimelineState createState() => _ProgramTimelineState(events, onEventPressed);
+  }
+
+  class _ProgramTimelineState extends State<ProgramTimeline> {
+
+    final List<EventModel> events;
+    Function(int)? onEventPressed;
+
+  _ProgramTimelineState(this.events, this.onEventPressed);
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Timeline.tileBuilder(
+        theme: TimelineTheme.of(context).copyWith(
+          nodePosition: 0.3,
+          indicatorTheme: IndicatorTheme.of(context).copyWith(
+              color: primaryBlue1
+          ),
+          connectorTheme: ConnectorTheme.of(context).copyWith(
+              color: primaryBlue1,
+              thickness: 2),
+        ),
+        builder: TimelineTileBuilder.connected(
+          itemCount: events.length,
+          contentsAlign: ContentsAlign.basic,
+          oppositeContentsBuilder: (_, index) {
+            final event = events[index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(event.startTimeString()),
+            );
+          },
+          contentsBuilder: (_, index) {
+            final event = events[index];
+            //return Text(event.maxParticipants == null ? event.title : "${event.title} (${event.currentParticipants}/${event.maxParticipants})");
+            return TextButton(
+                onPressed: () => onEventPressed!(event.id),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black, // Text Color
+                ),
+                child: Text(event.toString()));
+          },
+          indicatorBuilder: (_, index) {
+            final event = events[index];
+            return OutlinedDotIndicator(color: primaryBlue1, borderWidth: event.isSignedIn ? 6:2);
+          },
+          connectorBuilder: (_,index,__) {
+            if(index == events.length-1)
+            {
+              return const TransparentConnector();
+            }
+            return const SolidLineConnector();
+          },
+        ),
+      );
+    }
+  }
