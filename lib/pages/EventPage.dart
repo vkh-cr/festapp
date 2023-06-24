fd5337f485dc7fe27e4acc7d179dce7d0c21eaf1
miakh <2659269+miakh@users.noreply.github.com>
2023-06-24 22:43:46 +0200

    sign out on user event search

--- lib/pages/EventPage.dart
+++ lib/pages/EventPage.dart
@@ -19,6 +19,7 @@ class _EventPageState extends State<EventPage> {
   List<ParticipantModel> _queriedParticipants = [];
   int eventId;
   bool isLoadingParticipants = true;
+
   _EventPageState(this.eventId);
 
   @override
@@ -33,59 +34,84 @@ class _EventPageState extends State<EventPage> {
       appBar: AppBar(
         title: Text(_event.toString()),
       ),
-      body:
-      Column(
+      body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                  Visibility(visible: showLoginLogoutButton() && !_participants.any((p)=>DataService.currentUserEmail()==p.email),
-                      child: ElevatedButton(onPressed: () => signIn(), child: const Text("Přihlásit se"))),
-                  Visibility(visible: showLoginLogoutButton() && _participants.any((p)=>DataService.currentUserEmail()==p.email),
-                      child: ElevatedButton(onPressed: () => signOut(), child: const Text("Odhlásit se"))),
-                  Visibility(
-                    visible: showLoginLogoutButton(),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(onPressed: () async {
+            child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              Visibility(
+                  visible: showLoginLogoutButton() &&
+                      !_participants.any(
+                          (p) => DataService.currentUserEmail() == p.email),
+                  child: ElevatedButton(
+                      onPressed: () => signIn(),
+                      child: const Text("Přihlásit se"))),
+              Visibility(
+                  visible: showLoginLogoutButton() &&
+                      _participants.any(
+                          (p) => DataService.currentUserEmail() == p.email),
+                  child: ElevatedButton(
+                      onPressed: () => signOut(),
+                      child: const Text("Odhlásit se"))),
+              Visibility(
+                visible: showLoginLogoutButton(),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () async {
                         _queriedParticipants = await DataService.getAllUsers();
-                        _queriedParticipants.removeWhere((q) => _participants.any((p) => p.email == q.email));
+                        _queriedParticipants.forEach((q) => {
+                              if (_participants.any((p) => p.email == q.email))
+                                {q.isSignedIn = true}
+                            });
+
                         // ignore: use_build_context_synchronously
                         showSearch(
-                          context: context,
-                          delegate: SearchPage<ParticipantModel>(
-                            showItemsOnEmpty: true,
-                            items: _queriedParticipants,
-                            searchLabel: 'Hledat účastníky',
-                            suggestion: const Center(
-                              child: Text("Najdi účastníka podle jména, příjmení nebo e-mailu."),
-                            ),
-                            failure: const Center(
-                              child: Text("Nikdo nebyl nalezen."),
-                            ),
-                            filter: (person) => [
-                              person.name,
-                              person.surname,
-                              person.email,
-                            ],
-                            builder: (person) => ListTile(
-                              title: Text(person.name),
-                              subtitle: Text(person.surname),
-                              trailing: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  ElevatedButton(onPressed: () => signIn(person.email), child: const Text("Přihlásit")),
-                                  Text(person.email),
-                                ],
+                            context: context,
+                            delegate: SearchPage<ParticipantModel>(
+                              showItemsOnEmpty: true,
+                              items: _queriedParticipants,
+                              searchLabel: 'Hledat účastníky',
+                              suggestion: const Center(
+                                child: Text(
+                                    "Najdi účastníka podle jména, příjmení nebo e-mailu."),
                               ),
-                            ),
-                          )).then((x) => loadData());
-                      }, child: const Text("Přihlásit druhého")),
-                    ),
-                  ),
-                  ]),
+                              failure: const Center(
+                                child: Text("Nikdo nebyl nalezen."),
+                              ),
+                              filter: (person) => [
+                                person.name,
+                                person.surname,
+                                person.email,
+                              ],
+                              builder: (person) => ListTile(
+                                title: Text(person.name),
+                                subtitle: Text(person.surname),
+                                trailing: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Visibility(
+                                      visible: person.isSignedIn?true:false,
+                                      child: ElevatedButton(
+                                          onPressed: () => signOutOther(person),
+                                          child: const Text("Odhlásit")),
+                                    ),
+                                    Visibility(
+                                      visible: person.isSignedIn?false:true,
+                                      child: ElevatedButton(
+                                          onPressed: () => signIn(person),
+                                          child: const Text("Přihlásit")),
+                                    ),
+                                    Text(person.email),
+                                  ],
+                                ),
+                              ),
+                            )).then((x) => loadData());
+                      },
+                      child: const Text("Přihlásit druhého")),
+                ),
+              ),
+            ]),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -98,30 +124,37 @@ class _EventPageState extends State<EventPage> {
               visible: !DataService.isLoggedIn(),
               child: const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Text("Na tuto událost je nutné se přihlásit. Se svým e-mailem se přihlašte do aplikace, případně využijte možnosti přihlásit se na recepci."),
+                child: Text(
+                    "Na tuto událost je nutné se přihlásit. Se svým e-mailem se přihlašte do aplikace, případně využijte možnosti přihlásit se na recepci."),
               )),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(_event.description ?? ""),
           ),
           Visibility(
-              visible: DataService.isLoggedIn() && _event.maxParticipants != null,
-              child: Container(alignment: Alignment.topLeft, padding: const EdgeInsets.all(8.0), child: const Text("Přihlášeni:"))),
+              visible:
+                  DataService.isLoggedIn() && _event.maxParticipants != null,
+              child: Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text("Přihlášeni:"))),
           Expanded(
             child: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: _participants.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    IconButton(onPressed: () => signOutOther(_participants[index]), icon: const Icon(Icons.remove_circle_outline)),
-                    Text("${_participants[index]}"),
-                  ],
-                ),
-              );
-            }),
+                padding: const EdgeInsets.all(8),
+                itemCount: _participants.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        IconButton(
+                            onPressed: () => signOutOther(_participants[index]),
+                            icon: const Icon(Icons.remove_circle_outline)),
+                        Text("${_participants[index]}"),
+                      ],
+                    ),
+                  );
+                }),
           ),
         ],
       ),
@@ -129,7 +162,9 @@ class _EventPageState extends State<EventPage> {
   }
 
   bool showLoginLogoutButton() {
-    return DataService.isLoggedIn() && !isLoadingParticipants && _event.canSignIn();
+    return DataService.isLoggedIn() &&
+        !isLoadingParticipants &&
+        _event.canSignIn();
   }
 
   Future<void> loadData() async {
@@ -137,26 +172,30 @@ class _EventPageState extends State<EventPage> {
   }
 
   Future<void> loadParticipants() async {
-    if(!DataService.isLoggedIn()){
+    if (!DataService.isLoggedIn()) {
       return;
     }
     var participants = await DataService.getParticipantsPerEvent(eventId);
-    _participants = List.from(participants.map((par) => ParticipantModel(par["email"], par["migrated_users"]["name"], par["migrated_users"]["surname"])));
+    _participants = List.from(participants.map((par) => ParticipantModel(
+        par["email"],
+        par["migrated_users"]["name"],
+        par["migrated_users"]["surname"])));
     isLoadingParticipants = false;
-    setState(()=>{});
+    setState(() => {});
   }
 
   Future<void> loadEvent() async {
     var event = await DataService.getEvent(eventId);
-    var currentParticipants = await DataService.getParticipantsPerEventCount(eventId);
+    var currentParticipants =
+        await DataService.getParticipantsPerEventCount(eventId);
     setState(() {
       _event = EventModel.fromJson(eventId, event);
       _event.currentParticipants = currentParticipants;
     });
   }
 
-  Future<void> signIn([String? email]) async {
-    await DataService.signInToEvent(eventId, email);
+  Future<void> signIn([ParticipantModel? participant]) async {
+    await DataService.signInToEvent(eventId, participant?.email);
     await loadData();
   }
 
@@ -165,7 +204,7 @@ class _EventPageState extends State<EventPage> {
     await loadData();
   }
 
-  Future<String?> signOutOther(ParticipantModel? participant) {
+  Future<String?> signOutOther(ParticipantModel participant) {
     return showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
@@ -179,7 +218,7 @@ class _EventPageState extends State<EventPage> {
           TextButton(
             onPressed: () async {
               Navigator.pop(context, 'Odhlásit');
-              await DataService.signOutFromEvent(eventId, participant?.email);
+              await DataService.signOutFromEvent(eventId, participant.email);
               await loadData();
             },
             child: const Text('Odhlásit'),
