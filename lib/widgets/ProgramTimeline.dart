8e9fe9920964ba623c7e64709f6168df33f86b9c
miakh <2659269+miakh@users.noreply.github.com>
2023-07-25 06:12:23 +0200

    empty fix

--- lib/widgets/ProgramTimeline.dart
+++ lib/widgets/ProgramTimeline.dart
@@ -1,93 +1,107 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:timelines/timelines.dart';
 
 import '../models/EventModel.dart';
 import '../styles/Styles.dart';
 
-  class ProgramTimeline extends StatefulWidget {
-    Function(int)? onEventPressed;
+class ProgramTimeline extends StatefulWidget {
+  Function(int)? onEventPressed;
 
-    List<EventModel> events = [];
+  List<EventModel> events = [];
 
-   ProgramTimeline({super.key, required this.events, this.onEventPressed});
+  ProgramTimeline({super.key, required this.events, this.onEventPressed});
 
-    @override
-    _ProgramTimelineState createState() => _ProgramTimelineState(events, onEventPressed);
-  }
+  @override
+  _ProgramTimelineState createState() =>
+      _ProgramTimelineState(events, onEventPressed);
+}
 
-  class _ProgramTimelineState extends State<ProgramTimeline> {
-
-    final List<EventModel> allEvents;
-    Function(int)? onEventPressed;
+class _ProgramTimelineState extends State<ProgramTimeline> {
+  final List<EventModel> allEvents;
+  Function(int)? onEventPressed;
 
   _ProgramTimelineState(this.allEvents, this.onEventPressed);
 
-
-    @override
-    Widget build(BuildContext context) {
-      var morningEvents = allEvents.where((e) => e.startTime.hour <= 12).toList();
-      var afternoonEvents = allEvents.where((e) => e.startTime.hour > 12 && e.startTime.hour <= 18).toList();
-      var eveningEvents = allEvents.where((e) => e.startTime.hour>18).toList();
-      return SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+  @override
+  Widget build(BuildContext context) {
+    var morningEvents = allEvents.where((e) => e.startTime.hour <= 12).toList();
+    var afternoonEvents = allEvents
+        .where((e) => e.startTime.hour > 12 && e.startTime.hour <= 18)
+        .toList();
+    var eveningEvents = allEvents.where((e) => e.startTime.hour > 18).toList();
+    return SingleChildScrollView(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
         createTimeline(morningEvents),
-        const Padding(
-          padding: EdgeInsets.fromLTRB(64, 18, 0, 12),
-          child: Text("Odpoledne", style: TextStyle(color: primaryBlue1),),
-        ),
-        createTimeline(afternoonEvents),
-        const Padding(
-          padding: EdgeInsets.fromLTRB(64, 18, 0, 12),
-          child: Text("Večer", style: TextStyle(color: primaryBlue1),),
-        ),
-        createTimeline(eveningEvents),
-      ],));
-    }
-
-    Timeline createTimeline(List<EventModel> events)
-    {
-      return Timeline.tileBuilder(
-        shrinkWrap: true,
-        physics: ScrollPhysics(),
-        theme: TimelineTheme.of(context).copyWith(
-          nodePosition: 0.3,
-          indicatorTheme: IndicatorTheme.of(context).copyWith(
-              color: primaryBlue1
-          ),
-          connectorTheme: ConnectorTheme.of(context).copyWith(
-              color: primaryBlue1,
-              thickness: 2),
-        ),
-        builder: TimelineTileBuilder.connected(
-          itemCount: events.length,
-          contentsAlign: ContentsAlign.basic,
-          oppositeContentsBuilder: (_, index) {
-            final event = events[index];
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(event.startTimeString()),
-            );
-          },
-          contentsBuilder: (_, index) {
-            final event = events[index];
-            //return Text(event.maxParticipants == null ? event.title : "${event.title} (${event.currentParticipants}/${event.maxParticipants})");
-            return TextButton(
-                onPressed: () => onEventPressed!(event.id),
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black, // Text Color
+        afternoonEvents.isNotEmpty
+            ? const Padding(
+                padding: EdgeInsets.fromLTRB(64, 18, 0, 12),
+                child: Text(
+                  "Odpoledne",
+                  style: TextStyle(color: primaryBlue1),
                 ),
-                child: Text(event.toString()));
-          },
-          indicatorBuilder: (_, index) {
-            final event = events[index];
-            return EventModel.canSignIn(event) ? OutlinedDotIndicator(color: primaryBlue1, borderWidth: event.isSignedIn ? 6:2) : Padding(padding: EdgeInsetsDirectional.symmetric(horizontal: 3.5), child: DotIndicator(color: primaryBlue1, size: 8));
-          },
-          connectorBuilder: (_,index,__) {
-            return const SolidLineConnector();
-          },
-        ),
-      );
-    }
+              )
+            : const SizedBox.shrink(),
+        createTimeline(afternoonEvents),
+        eveningEvents.isNotEmpty
+            ? const Padding(
+                padding: EdgeInsets.fromLTRB(64, 18, 0, 12),
+                child: Text(
+                  "Večer",
+                  style: TextStyle(color: primaryBlue1),
+                ),
+              )
+            : const SizedBox.shrink(),
+        createTimeline(eveningEvents),
+      ],
+    ));
   }
+
+  Timeline createTimeline(List<EventModel> events) {
+    return Timeline.tileBuilder(
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      theme: TimelineTheme.of(context).copyWith(
+        nodePosition: 0.3,
+        indicatorTheme:
+            IndicatorTheme.of(context).copyWith(color: primaryBlue1),
+        connectorTheme: ConnectorTheme.of(context)
+            .copyWith(color: primaryBlue1, thickness: 2),
+      ),
+      builder: TimelineTileBuilder.connected(
+        itemCount: events.length,
+        contentsAlign: ContentsAlign.basic,
+        oppositeContentsBuilder: (_, index) {
+          final event = events[index];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(event.startTimeString()),
+          );
+        },
+        contentsBuilder: (_, index) {
+          final event = events[index];
+          //return Text(event.maxParticipants == null ? event.title : "${event.title} (${event.currentParticipants}/${event.maxParticipants})");
+          return TextButton(
+              onPressed: () => onEventPressed!(event.id),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black, // Text Color
+              ),
+              child: Text(event.toString()));
+        },
+        indicatorBuilder: (_, index) {
+          final event = events[index];
+          return EventModel.canSignIn(event)
+              ? OutlinedDotIndicator(
+                  color: primaryBlue1, borderWidth: event.isSignedIn ? 6 : 2)
+              : const Padding(
+                  padding: EdgeInsetsDirectional.symmetric(horizontal: 3.5),
+                  child: DotIndicator(color: primaryBlue1, size: 8));
+        },
+        connectorBuilder: (_, index, __) {
+          return const SolidLineConnector();
+        },
+      ),
+    );
+  }
+}
