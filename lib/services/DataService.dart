21bd5e1213b01ffe05f122da3ad13adf77ca34db
miakh <2659269+miakh@users.noreply.github.com>
2023-08-16 12:13:59 +0200

    version up

--- lib/services/DataService.dart
+++ lib/services/DataService.dart
@@ -648,7 +648,7 @@ class DataService {
         }
         return;
       }
-      case 104: ToastHelper.Show("Nelze přihlásit! Přihlašovat se můžeš až od čtvrtku 17.8.2023 9:00.", severity: ToastSeverity.NotOk); return;
+      case 104: ToastHelper.Show("Nelze přihlásit! Přihlašovat se můžeš od čtvrtku 17.8.2023 9:00.", severity: ToastSeverity.NotOk); return;
       case 105: ToastHelper.Show("Nelze přihlásit! Na událost už je přihlášeno maximum mužů.", severity: ToastSeverity.NotOk); return;
       case 106: ToastHelper.Show("Nelze přihlásit! Na událost už je přihlášeno maximum žen.", severity: ToastSeverity.NotOk); return;
     }
