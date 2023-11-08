09042123b3ef5b7d7d199c571403899097e62400
miakh <2659269+miakh@users.noreply.github.com>
2023-11-08 12:12:02 +0100

    merge

--- lib/config.dart
+++ lib/config.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class configuration {
+class config {
   static const String supabase_url = 'https://icdqmvvcvctrulmpucij.supabase.co';
   static const String anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljZHFtdnZjdmN0cnVsbXB1Y2lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2MTc4OTAsImV4cCI6MjAxMjE5Mzg5MH0.appaX_mPTS-yxJVXoezP9Wo6viRV9KUQ9OIEahnTidE';
   static const String home_page = 'FotoFest 2023';
   static const String map_page = 'Mapa FotoFestu 2023';
 
-  static const primaryColor = 0xFF2C677B;
+  static const primaryColor = 0xFF122640;
   static const backgroundColor = Color(0xFFF2C572);
   static const color1 = Color(primaryColor);
   static const color2 = Color(0xFFBF8641);
