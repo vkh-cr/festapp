561c619fac71ce5a4a2c7d37472e635b546df0ff
miakh <2659269+miakh@users.noreply.github.com>
2023-11-16 00:05:53 +0100

    db functions rename

--- Database/01_claims_functions.sql
+++ Database/01_claims_functions.sql
@@ -111,3 +111,4 @@ NOTIFY pgrst, 'reload schema';
 -- DROP FUNCTION is_claims_admin;
 -- NOTIFY pgrst, 'reload schema';
 --select set_claim('9e311817-3215-4454-91d3-ec33e2df1257', 'is_admin', 'true');
+--select set_claim('25651d62-2649-4b95-9a16-8ba86efd01d4', 'is_editor', 'true');
