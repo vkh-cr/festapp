a1049cd25a42eee043f89d85c59206404d17c156
miakh <2659269+miakh@users.noreply.github.com>
2024-01-23 17:06:40 +0100

    fontweight bold

--- lib/widgets/Timetable.dart
+++ lib/widgets/Timetable.dart
@@ -76,7 +76,7 @@ class _TimeTableState extends State<TimeTable> {
                       alignment: Alignment.centerLeft,
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Text(widget.timetablePlaces[i].title, style: TextStyle(fontWeight: FontWeight.bold),),
+                        child: Text(widget.timetablePlaces[i].title, style: const TextStyle(fontWeight: FontWeight.bold),),
                       ),
                     ),
                   )),
