e54e7d52f8d8ca73541b071415a70b346ad94dc6
miakh <2659269+miakh@users.noreply.github.com>
2024-03-24 11:26:09 +0100

    config update

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'models/LanguageModel.dart';
 
 class AppConfig {
-  static const String supabase_url = 'https://kjdpmixlnhntmxjedpxh.supabase.co';
-  static const String anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZHBtaXhsbmhudG14amVkcHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NDI5NzEsImV4cCI6MjAxNzUxODk3MX0.06nTXCL-i1GxLckfEyCNlVVwt62QTzKUezqmsYSR_MI';
+  static const String supabase_url = 'https://lwfpdjxsdmkfyrzqbrlk.supabase.co';
+  static const String anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3ZnBkanhzZG1rZnlyenFicmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjgwNDksImV4cCI6MjAyNjgwNDA0OX0.440P3yf8ZqNjOHEQTn9uJhkKyObhD_7PZ2NwaE9e4J0';
   static const String home_page = 'CSA 2024';
   static String map_page = "Map".tr();
 
