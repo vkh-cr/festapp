cb0fa3f05be1ef5d2874a47068786beaa5091dbe
miakh <2659269+miakh@users.noreply.github.com>
2024-04-10 21:20:42 +0200

    config install option

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -36,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     List<Widget> actions = [const LanguageButton()];
-    if(PWAInstall().installPromptEnabled){
+    if(AppConfig.showPWAInstallOption && PWAInstall().installPromptEnabled){
       actions.add(ButtonsHelper.pwaInstallButton());
     }
 
