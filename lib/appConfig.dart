6b3c336b0d2f725d1540f5ca8b0ec75cb84bde0b
miakh <2659269+miakh@users.noreply.github.com>
2024-06-15 14:58:16 +0200

    removed prefix on password

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -13,7 +13,6 @@ class AppConfig {
   static const bool isNotificationsSupported = true;
   static const bool isUsersImportSupported = true;
   static const String oneSignalAppId = '0b9c568e-1231-4a5d-a82e-06622def39f4';
-  static const String generatedPasswordPrefix = "fa";
   static const String defaultLink = "conference2024";
 
   //frosty style
