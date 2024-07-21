20637f56d6faff034bf57bae1b511a287977d66d
miakh <miakh@users.noreply.github.com>
2024-07-21 00:05:35 +0000

    deploy: 44b30b792638339b8530cb352699a7ed58f4c172

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1707245780"
+    serviceWorkerVersion: "3591461539"
   }
 });
