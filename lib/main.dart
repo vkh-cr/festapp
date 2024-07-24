9c46136e3ad8a8239401d863b7b7ee9fb559afcc
miakh <2659269+miakh@users.noreply.github.com>
2024-07-24 21:31:24 +0200

    tst change

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:fstapp/RouterService.dart';
 import 'package:fstapp/pages/HomePage.dart';
 import 'package:fstapp/services/NotificationHelper.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:fstapp/services/TimeHelper.dart';
