83c7c48309b788f13faa1e20487f5d4065c19bdd
miakh <2659269+miakh@users.noreply.github.com>
2024-07-31 18:27:10 +0200

    when navigating to homepage, then refresh

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -32,21 +32,10 @@ import 'package:fstapp/pages/MySchedulePage.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 
-
-
 class HomePage extends StatefulWidget {
   static const HOME_PAGE = AppConfig.appName;
 
   const HomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -85,12 +74,6 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         body: SafeArea(
           child: Column(
@@ -161,6 +144,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                   )),
               Expanded(
                   child: ScheduleTabView(
+                    key: UniqueKey(),
                     events: _dots,
                     onEventPressed: _eventPressed,
                   )),
