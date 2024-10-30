a5454641ffd3582a063f6842e07cd8cde184f9fd
miakh <2659269+miakh@users.noreply.github.com>
2024-10-30 19:32:58 +0100

    white style

--- lib/main.dart
+++ lib/main.dart
@@ -170,7 +170,28 @@ class _MyAppState extends State<MyApp> {
             unselectedItemColor: Colors.black26,
           ),
           colorScheme: ColorScheme.fromSwatch(primarySwatch: primarySwatch)
-              .copyWith(surface: AppConfig.backgroundColor)),
+              .copyWith(
+            surface: AppConfig.backgroundColor,
+            onSurface: Colors.white,
+            onPrimary: Colors.white,
+          ),
+          textTheme: TextTheme(
+            bodyLarge: TextStyle(color: Colors.white),
+            bodyMedium: TextStyle(color: Colors.white),
+            bodySmall: TextStyle(color: Colors.white),
+            displayLarge: TextStyle(color: Colors.white),
+            displayMedium: TextStyle(color: Colors.white),
+            displaySmall: TextStyle(color: Colors.white),
+            headlineLarge: TextStyle(color: Colors.white),
+            headlineMedium: TextStyle(color: Colors.white),
+            headlineSmall: TextStyle(color: Colors.white),
+            labelLarge: TextStyle(color: Colors.white),
+            labelMedium: TextStyle(color: Colors.white),
+            labelSmall: TextStyle(color: Colors.white),
+            titleLarge: TextStyle(color: Colors.white),
+            titleMedium: TextStyle(color: Colors.white),
+            titleSmall: TextStyle(color: Colors.white),
+          )),
     ).animate().fadeIn(
       duration: 300.ms,
     );
