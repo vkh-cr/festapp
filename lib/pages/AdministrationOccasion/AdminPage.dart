c1bde7babb24e918ae3107cf1ddebbdabfb9725d
miakh <2659269+miakh@users.noreply.github.com>
2024-11-02 21:03:19 +0100

    administrator page refactor

---
+++ lib/pages/AdministrationOccasion/AdminPage.dart
@@ -0,0 +1,145 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:fstapp/RouterService.dart';
+import 'package:fstapp/dataModels/IconModel.dart';
+import 'package:fstapp/dataModels/OccasionModel.dart';
+import 'package:fstapp/dataModels/PlaceModel.dart';
+import 'package:fstapp/dataServices/DbPlaces.dart';
+import 'package:fstapp/dataServices/DbUsers.dart';
+import 'package:fstapp/dataServices/RightsService.dart';
+import 'package:fstapp/pages/AdministrationOccasion/EventsTab.dart';
+import 'package:fstapp/pages/AdministrationOccasion/ExclusivityTab.dart';
+import 'package:fstapp/pages/AdministrationOccasion/InformationTab.dart';
+import 'package:fstapp/pages/AdministrationOccasion/PlacesTab.dart';
+import 'package:fstapp/pages/AdministrationOccasion/UserGroupsTab.dart';
+import 'package:fstapp/pages/AdministrationOccasion/UsersTab.dart';
+
+@RoutePage()
+class AdminPage extends StatefulWidget {
+  static const ROUTE = "admin";
+  const AdminPage({Key? key}) : super(key: key);
+
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  // Shared Data
+  OccasionModel? occasionModel;
+  List<String> places = [];
+  List<int?> mapIcons = [];
+  List<IconModel> svgIcons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 6);
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    // Load data for all tabs
+    occasionModel = await DbUsers.getOccasion(RightsService.currentOccasion!);
+    await loadPlaces();
+    var svgs = await DbPlaces.getAllIcons();
+    svgIcons.clear();
+    svgIcons.addAll(svgs);
+    mapIcons.clear();
+    mapIcons.addAll(svgIcons.map((icon) => icon.id).toList());
+    mapIcons.add(null);
+
+    setState(() {});
+  }
+
+  Future<void> loadPlaces() async {
+    var placesRaws = await DbPlaces.getMapPlaces();
+    var placesStrings = placesRaws.map((p) => p.toPlutoSelectString()).toList();
+    placesStrings.add(PlaceModel.WithouValue);
+    places.clear();
+    places.addAll(placesStrings);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _tabController.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Admin").tr(),
+          leading: BackButton(
+            onPressed: () => RouterService.popOrHome(context),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(40),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                tabs: [
+                  Row(
+                      children: [
+                        const Icon(Icons.info),
+                        Padding(padding: const EdgeInsets.all(12), child: const Text("Info").tr())
+                      ]
+                  ),
+                  Row(
+                      children: [
+                        const Icon(Icons.calendar_month),
+                        Padding(padding: const EdgeInsets.all(12), child: const Text("Events").tr())
+                      ]
+                  ),
+                  Row(
+                      children: [
+                        const Icon(Icons.pin_drop),
+                        Padding(padding: const EdgeInsets.all(12), child: const Text("Places").tr())
+                      ]
+                  ),
+                  Row(
+                      children: [
+                        const Icon(Icons.punch_clock_rounded),
+                        Padding(padding: const EdgeInsets.all(12), child: const Text("Exclusivity").tr())
+                      ]
+                  ),
+                  Row(
+                      children: [
+                        const Icon(Icons.groups),
+                        Padding(padding: const EdgeInsets.all(12), child: const Text("Groups").tr())
+                      ]
+                  ),
+                  Row(
+                      children: [
+                        const Icon(Icons.people),
+                        Padding(padding: const EdgeInsets.all(12), child: const Text("Users").tr())
+                      ]
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            InformationTab(),
+            EventsTab(occasionModel: occasionModel, places: places),
+            PlacesTab(svgIcons: svgIcons, mapIcons: mapIcons, loadPlaces: loadPlaces,),
+            ExclusivityTab(),
+            UserGroupsTab(),
+            UsersTab(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/pages/AdministrationOccasion/PlacesTab.dart
@@ -0,0 +1,110 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:fstapp/AppRouter.gr.dart';
+import 'package:fstapp/RouterService.dart';
+import 'package:fstapp/components/dataGrid/DataGridAction.dart';
+import 'package:fstapp/components/dataGrid/DataGridHelper.dart';
+import 'package:fstapp/dataModels/IconModel.dart';
+import 'package:fstapp/dataModels/PlaceModel.dart';
+import 'package:fstapp/components/dataGrid/SingleTableDataGrid.dart';
+import 'package:fstapp/dataModels/Tb.dart';
+import 'package:fstapp/dataServices/DbPlaces.dart';
+import 'package:fstapp/dataServices/SynchroService.dart';
+import 'package:pluto_grid_plus/pluto_grid_plus.dart';
+
+class PlacesTab extends StatelessWidget {
+  final List<IconModel> svgIcons;
+  final List<int?> mapIcons;
+  final Future<void> Function() loadPlaces;
+
+  const PlacesTab({Key? key, required this.svgIcons, required this.mapIcons, required this.loadPlaces}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleTableDataGrid<PlaceModel>(
+      context,
+      DbPlaces.getAllPlaces,
+      PlaceModel.fromPlutoJson,
+      DataGridFirstColumn.deleteAndDuplicate,
+      Tb.places.id,
+      actionsExtended: DataGridExtendedActions(saveAction:
+      DataGridAction(
+          action: (datagrid, [action]) async {
+            await action!();
+            await loadPlaces();
+          }
+      )),
+      columns: [
+        PlutoColumn(
+          hide: true,
+          title: "Id".tr(),
+          field: Tb.places.id,
+          type: PlutoColumnType.number(defaultValue: -1),
+          readOnly: true,
+          width: 50,
+          renderer: (rendererContext) => DataGridHelper.idRenderer(rendererContext),
+        ),
+        PlutoColumn(
+          title: "Hide".tr(),
+          field: Tb.places.is_hidden,
+          type: PlutoColumnType.select([]),
+          applyFormatterInEditing: true,
+          enableEditingMode: false,
+          width: 100,
+          renderer: (rendererContext) => DataGridHelper.checkBoxRenderer(rendererContext, Tb.places.is_hidden),
+        ),
+        PlutoColumn(
+            title: "Title".tr(),
+            field: Tb.places.title,
+            type: PlutoColumnType.text(),
+            width: 300
+        ),
+        PlutoColumn(
+            title: "Content".tr(),
+            field: Tb.places.description,
+            type: PlutoColumnType.text(),
+            width: 300
+        ),
+        PlutoColumn(
+          title: "Icon".tr(),
+          field: Tb.places.icon,
+          type: PlutoColumnType.select(mapIcons, builder: (icon) {
+            return DataGridHelper.iconToRow(context, icon, svgIcons);
+          }),
+          renderer: (rendererContext) => DataGridHelper.mapIconRenderer(context, rendererContext, svgIcons),
+        ),
+        PlutoColumn(
+            width: 150,
+            title: "Location on map".tr(),
+            enableFilterMenuItem: false,
+            enableContextMenu: false,
+            enableSorting: false,
+            field: Tb.places.coordinates,
+            type: PlutoColumnType.text(defaultValue: SynchroService.globalSettingsModel!.defaultMapLocation),
+            renderer: (rendererContext) {
+              return ElevatedButton(
+                  onPressed: () async {
+                    var placeModel = PlaceModel.fromPlutoJson(rendererContext.row.toJson());
+                    RouterService.navigatePageInfo(context, MapRoute(place: placeModel)).then((value) async {
+                      if(value != null)
+                      {
+                        var cell = rendererContext.row.cells[Tb.places.coordinates]!;
+                        rendererContext.stateManager.changeCellValue(cell, value, force: true);
+                      }
+                    });
+                  },
+                  child: Row(children: [const Icon(Icons.edit), Padding(padding: const EdgeInsets.all(6), child: const Text("Edit").tr()) ])
+              );
+            }),
+        PlutoColumn(
+          title: "Order".tr(),
+          field: Tb.places.order,
+          type: PlutoColumnType.number(defaultValue: null),
+          applyFormatterInEditing: true,
+          width: 100,
+        ),
+        // Other columns for PlacesTab
+      ],
+    ).DataGrid();
+  }
+}
