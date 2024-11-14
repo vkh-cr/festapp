51ac28b759e4a99da6d917b481657277908f9df7
miakh <2659269+miakh@users.noreply.github.com>
2024-11-14 10:29:52 +0100

    isGameSupported

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -14,6 +14,7 @@ class AppConfig {
   static const bool isNotificationsSupported = true;
   static const bool isWebNotificationsSupported = true;
   static const bool isUsersImportSupported = true;
+  static const bool isGameSupported = true;
   static const String oneSignalAppId = '73f77f22-961a-4ded-9647-e33a7ac14f90';
 
   static const int organization = 1;
