539bada9246d115d11f8f37c83ae2703a19460e7
miakh <2659269+miakh@users.noreply.github.com>
2024-11-14 13:57:36 +0100

    droid adjust

--- android/app/src/main/kotlin/festapp/fstapp/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-<<<<<<<< HEAD:android/app/src/main/kotlin/festapp/ffapp/MainActivity.kt
-package vkhcr.ffapp
-========
-package fstapp.fstapp
->>>>>>>> main:android/app/src/main/kotlin/festapp/fstapp/MainActivity.kt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
