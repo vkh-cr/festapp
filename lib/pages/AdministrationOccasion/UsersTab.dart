f5d0bde6827e9682187512bbef51fb2a6d1632e4
miakh <2659269+miakh@users.noreply.github.com>
2024-11-21 16:56:07 +0100

    delay fix

--- lib/pages/AdministrationOccasion/UsersTab.dart
+++ lib/pages/AdministrationOccasion/UsersTab.dart
@@ -204,6 +204,7 @@ class _UsersTabState extends State<UsersTab> {
                 print(
                     "Retrying to invite user: ${user.data![Tb.occasion_users.data_email]}. Attempt: ${retryAttempts[user]}");
               }
+              Future.delayed(Duration(milliseconds: 500));
             }
           }
         };
