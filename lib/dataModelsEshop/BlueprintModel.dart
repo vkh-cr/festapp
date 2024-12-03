ec849a98d9ff92fa7b45215d9ca7a3094ff908bf
miakh <2659269+miakh@users.noreply.github.com>
2024-12-03 15:30:45 +0100

    quick save

--- lib/dataModelsEshop/BlueprintModel.dart
+++ lib/dataModelsEshop/BlueprintModel.dart
@@ -7,6 +7,7 @@ import 'package:collection/collection.dart';
 
 class BlueprintModel {
   static const String metaSpots = "spots";
+  static const String metaDefaultProduct = "default_product";
   static const String metaTableAreaType = "table";
   static const String metaSpotType = "spot";
 
@@ -16,12 +17,35 @@ class BlueprintModel {
   String? title;
   int? organization;
   int? occasion;
+  int? defaultProduct;
   BlueprintConfiguration? configuration;
   List<BlueprintObjectModel>? objects;
   List<BlueprintObjectModel>? spots;
   List<BlueprintGroupModel>? groups;
 
   factory BlueprintModel.fromJson(Map<String, dynamic> json) {
+    List<BlueprintGroupModel>? groups = json[TbEshop.blueprints.groups] != null
+        ? List<BlueprintGroupModel>.from(
+        json[TbEshop.blueprints.groups].map((g) => BlueprintGroupModel.fromJson(g)))
+        : [];
+
+    List<BlueprintObjectModel>? objects = json[TbEshop.blueprints.objects] != null
+        ? List<BlueprintObjectModel>.from(
+        json[TbEshop.blueprints.objects].map((obj) => BlueprintObjectModel.fromJson(obj)))
+        : null;
+
+    // Assign objects to groups
+    if (objects != null) {
+      for (var obj in objects) {
+        if (obj.groupId != null) {
+          final group = groups.firstWhereOrNull((g) => g.id == obj.groupId);
+          if (group != null) {
+            group.objects.add(obj);
+          }
+        }
+      }
+    }
+
     return BlueprintModel(
       id: json[TbEshop.blueprints.id],
       createdAt: json[TbEshop.blueprints.created_at] != null
@@ -31,23 +55,19 @@ class BlueprintModel {
       title: json[TbEshop.blueprints.title],
       organization: json[TbEshop.blueprints.organization],
       occasion: json[TbEshop.blueprints.occasion],
+      defaultProduct: json[BlueprintModel.metaDefaultProduct],
       configuration: json[TbEshop.blueprints.configuration] != null
           ? BlueprintConfiguration.fromJson(json[TbEshop.blueprints.configuration])
           : null,
-      objects: json[TbEshop.blueprints.objects] != null
+      objects: objects,
+      spots: json[BlueprintModel.metaSpots] != null
           ? List<BlueprintObjectModel>.from(
-          json[TbEshop.blueprints.objects].map((obj) => BlueprintObjectModel.fromJson(obj)))
+          json[BlueprintModel.metaSpots].map((spot) => BlueprintObjectModel.fromJson(spot)))
           : null,
-      spots: json[metaSpots] != null
-          ? List<BlueprintObjectModel>.from(
-          json[metaSpots].map((spot) => BlueprintObjectModel.fromJson(spot)))
-          : null,
-      groups: json[TbEshop.blueprints.groups] != null
-          ? List<BlueprintGroupModel>.from(
-          json[TbEshop.blueprints.groups].map((g) => BlueprintGroupModel.fromJson(g)))
-          : [],
+      groups: groups,
     );
   }
+
 
   Map<String, dynamic> toJson() => {
     TbEshop.blueprints.id: id,
@@ -71,6 +91,7 @@ class BlueprintModel {
         final matchingSpot = spots?.firstWhereOrNull((spot) => spot.id == object.id);
         return BlueprintObjectModel(
           id: object.id,
+          product: matchingSpot?.product,
           title: matchingSpot?.title ?? object.title,
           stateEnum: BlueprintObjectModel.States.entries
               .firstWhereOrNull(
@@ -99,9 +120,18 @@ class BlueprintModel {
     this.title,
     this.organization,
     this.occasion,
+    this.defaultProduct,
     this.configuration,
     this.objects,
     this.spots,
     this.groups,
   });
+
+  int getFirstAvailableGroupId() {
+    int id = 1;
+    while (groups?.any((group) => group.id == id)??false) {
+      id++;
+    }
+    return id;
+  }
 }
