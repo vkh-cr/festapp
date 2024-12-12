126370aa149db6ba3c6f96a7b3a19dd36d4cb116
miakh <2659269+miakh@users.noreply.github.com>
2024-12-12 06:42:00 +0100

    utilities fix

--- lib/services/Utilities.dart
+++ lib/services/Utilities.dart
@@ -135,10 +135,9 @@ class Utilities {
     {'base':'z','letters':'\u007A\u24E9\uFF5A\u017A\u1E91\u017C\u017E\u1E93\u1E95\u01B6\u0225\u0240\u2C6C\uA763'}
   ];
 
-  /// Optimized for one call
+  static final diacriticsMap = {};
   static String removeDiacritics(String str) {
     final diacriticsRegExp = RegExp('[^\u0000-\u007E]', multiLine: true);
-    final diacriticsMap = {};
     if(diacriticsMap.isEmpty) {
       var map = defaultDiacriticsRemovalMap();
       for (int i = 0; i < map.length; i++){
