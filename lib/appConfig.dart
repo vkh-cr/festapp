dbc09ecee7667a556485d1ec786fffa262b22d22
miakh <2659269+miakh@users.noreply.github.com>
2024-12-20 00:36:10 +0100

    prod

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -18,7 +18,7 @@ class AppConfig {
   static const int organization = 4;
   //setup occasion id to force occasion
   static const String? forceOccasionLink = null;
-  static const bool isPublicNotificationSendingDisabled = true;
+  static const bool isPublicNotificationSendingDisabled = false;
 
   static const String webLink = "https://hvezdamorska.netlify.app";
   static const String appStoreLink = "https://apps.apple.com/us/app/festapp/id6474078383";
