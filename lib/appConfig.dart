943db0001ab90b7e4c6a5c41651999393ce54a7b
miakh <2659269+miakh@users.noreply.github.com>
2025-02-21 18:19:46 +0100

    test adjust

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:fstapp/dataModels/LanguageModel.dart';
 
 class AppConfig {
-  static const String supabaseUrl = 'https://lwfpdjxsdmkfyrzqbrlk.supabase.co';
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3ZnBkanhzZG1rZnlyenFicmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjgwNDksImV4cCI6MjAyNjgwNDA0OX0.440P3yf8ZqNjOHEQTn9uJhkKyObhD_7PZ2NwaE9e4J0';
+  static const String supabaseUrl = 'https://jxagtmxojljlkabacgcj.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4YWd0bXhvamxqbGthYmFjZ2NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4MTIxNTMsImV4cCI6MjA1NTM4ODE1M30.I9s25yl_dFBGFFGa3De9vbM3xlxkMzWRKPeg65oDsUo';
   static const String appName = 'Absolventský Velehrad';
   static String mapTitle = "Map".tr();
   static const bool showPWAInstallOption = true;
@@ -15,12 +15,12 @@ class AppConfig {
   static const bool isWebNotificationsSupported = true;
   static const String oneSignalAppId = '73f77f22-961a-4ded-9647-e33a7ac14f90';
 
-  static const int organization = 5;
+  static const int organization = 4;
   //setup occasion id to force occasion
   static const String? forceOccasionLink = null;
   static const bool isPublicNotificationSendingDisabled = true;
 
-  static const String webLink = "https://app.absolventskyvelehrad.cz";
+  static const String webLink = "https://avtest.netlify.app";
   static const String appStoreLink = "";
   static const String playStoreLink = "";
   static const String playStoreLaunchLink = "";
