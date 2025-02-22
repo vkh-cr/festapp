54cfd50d728684b1e5281c0bb8282be51be56988
miakh <2659269+miakh@users.noreply.github.com>
2025-02-22 02:58:40 +0100

    ext. form reservation

--- lib/pages/unit/OccasionSettingPage.dart
+++ lib/pages/unit/OccasionSettingPage.dart
@@ -15,6 +15,7 @@ import 'package:fstapp/services/DialogHelper.dart';
 import 'package:fstapp/services/ToastHelper.dart';
 import 'package:fstapp/services/Utilities.dart';
 import 'package:fstapp/themeConfig.dart';
+import 'package:fstapp/widgets/HelpWidget.dart';
 import 'package:fstapp/widgets/ImageArea.dart';
 import 'package:fstapp/widgets/OccasionCard.dart';
 import 'package:fstapp/widgets/TimeDataRangePicker.dart';
@@ -293,25 +294,8 @@ class _OccasionSettingsPageState extends State<OccasionSettingsPage> {
               SwitchListTile(
                 title: Row(
                   children: [
-                    Expanded(child: Text("Is Open".tr())),
-                    IconButton(
-                      icon: Icon(Icons.help_outline, color: ThemeConfig.blackColor(context),),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: Text("Is Open".tr()),
-                            content: Text("Determines whether event details (schedule, info, etc.) are available to the public.".tr()),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(),
-                                child: Text("Ok".tr()),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                    Expanded(child: Text("Public".tr())),
+                    HelpWidget(title: "Public".tr(), content: "Determines whether event details (schedule, info, etc.) are available to the public.".tr())
                   ],
                 ),
                 value: _isOpen,
