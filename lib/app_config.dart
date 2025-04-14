9d2f68a381e67a78a90dd0144831b48b67e98e3a
miakh <2659269+miakh@users.noreply.github.com>
2025-04-14 14:41:29 +0200

    all unit mode

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -19,6 +19,7 @@ class AppConfig {
   //setup occasion id to force occasion
   static const String? forceOccasionLink = null;
   static const bool isPublicNotificationSendingDisabled = false;
+  static const bool isAllUnit = false;
 
   static const String webLink = "https://live.festapp.net";
   static const String appStoreLink = "https://apps.apple.com/us/app/festapp/id6474078383";
