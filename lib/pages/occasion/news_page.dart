0756c97a2c5efdd431d0e70a277f5e2040f82892
miakh <2659269+miakh@users.noreply.github.com>
2025-05-06 17:52:47 +0200

    info page fix; youtube fix; refactor; 0.12.15+121

--- lib/pages/occasion/news_page.dart
+++ lib/pages/occasion/news_page.dart
@@ -14,6 +14,7 @@ import 'package:fstapp/widgets/html_view.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:fstapp/widgets/pop_button.dart';
+import 'package:fstapp/widgets/zoomable_image/zoomable_image.dart';
 import '../utility/html_editor_page.dart';
 
 @RoutePage()
