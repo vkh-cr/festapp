bdf334f1210228a65a7cca3cd0ee4dcf344ad267
miakh <2659269+miakh@users.noreply.github.com>
2025-05-23 04:51:13 +0200

    timezone support

---
+++ lib/services/time_utils/platform_timezone_web.dart
@@ -0,0 +1,14 @@
+// lib/src/platform_timezone_setup_web.dart
+import 'package:timezone/browser.dart' as tzb;
+import 'package:timezone/timezone.dart' as tz;
+
+
+class TimeZoneUniversal{
+  Future<void> initializeTimeZonePlatform() async {
+    await tzb.initializeTimeZone('assets/packages/timezone/data/latest.tzf');
+  }
+
+  void setTimeZoneLocationPlatform(tz.Location targetLocation) {
+    tzb.setLocalLocation(targetLocation);
+  }
+}
