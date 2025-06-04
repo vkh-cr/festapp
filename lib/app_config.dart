85d11e78954769471e81c58b3cb51b8205a3bf55
miakh <2659269+miakh@users.noreply.github.com>
2025-06-04 17:03:28 +0200

    version: 0.14.15+141; events inside activity; auto volunteer as approver

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -32,6 +32,8 @@ class AppConfig {
 
   static const int daySplitHour = 4;
 
+  static const bool areAllVolunteersApprovers = false;
+
   static bool isNotificationsCurrentlySupported() {
     if(kIsWeb) {
       return isNotificationsSupported && isWebNotificationsSupported;
