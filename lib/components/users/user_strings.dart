42e0b3db207097a001c16ab3a87c6e84030f6652
miakh <2659269+miakh@users.noreply.github.com>
2025-06-30 00:04:16 +0200

    version: 0.14.29+155; user tab adjustments

---
+++ lib/components/users/user_strings.dart
@@ -0,0 +1,35 @@
+import 'package:easy_localization/easy_localization.dart';
+
+class UserStrings {
+  // Column Titles
+  static String get id => 'UserColumns.id'.tr();
+  static String get unit => 'UserColumns.unit'.tr();
+  static String get email => 'UserColumns.email'.tr();
+  static String get name => 'UserColumns.name'.tr();
+  static String get surname => 'UserColumns.surname'.tr();
+  static String get sex => 'UserColumns.sex'.tr();
+  static String get accommodation => 'UserColumns.accommodation'.tr();
+  static String get phone => 'UserColumns.phone'.tr();
+  static String get birthday => 'UserColumns.birthday'.tr();
+  static String get role => 'UserColumns.role'.tr();
+  static String get text1 => 'UserColumns.text1'.tr();
+  static String get text2 => 'UserColumns.text2'.tr();
+  static String get text3 => 'UserColumns.text3'.tr();
+  static String get note => 'UserColumns.note'.tr();
+  static String get diet => 'UserColumns.diet'.tr();
+  static String get form => 'UserColumns.form'.tr();
+  static String get orderedAt => 'UserColumns.orderedAt'.tr();
+  static String get createdAt => 'UserColumns.createdAt'.tr();
+  static String get lastSignInAt => 'UserColumns.lastSignInAt'.tr();
+
+  // Status/Right Column Titles
+  static String get volunteer => 'UserColumns.volunteer'.tr();
+  static String get administrator => 'UserColumns.administrator'.tr();
+  static String get editor => 'UserColumns.editor'.tr();
+  static String get readOnly => 'UserColumns.readOnly'.tr();
+  static String get editOrders => 'UserColumns.editOrders'.tr();
+  static String get readOrders => 'UserColumns.readOrders'.tr();
+  static String get approver => 'UserColumns.approver'.tr();
+  static String get approved => 'UserColumns.approved'.tr();
+  static String get invited => 'UserColumns.invited'.tr();
+}
