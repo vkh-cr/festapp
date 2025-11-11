274c9d570b3f68870eed73b006954308777a705a
miakh <2659269+miakh@users.noreply.github.com>
2025-11-11 16:43:46 +0100

    version: 0.17.32+221; blueprint refact

--- lib/components/forms/models/form_holder.dart
+++ lib/components/forms/models/form_holder.dart
@@ -2,7 +2,7 @@ import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:fstapp/components/seat_reservation/model/seat_model.dart';
+import 'package:fstapp/components/blueprint/seat_reservation/model/seat_model.dart';
 import 'package:fstapp/data_models/form_model.dart';
 import 'package:fstapp/data_models/form_option_product_model.dart';
 import 'field_holder.dart';
