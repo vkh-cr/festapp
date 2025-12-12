55f46d7ec3615b99c15b16a869866271c5891d27
miakh <2659269+miakh@users.noreply.github.com>
2025-12-12 21:33:48 +0100

    version: 0.18.1+237; e-mail banner; unit email templates and banner

--- lib/components/email_templates/views/email_templates_settings_page.dart
+++ lib/components/email_templates/views/email_templates_settings_page.dart
@@ -5,12 +5,15 @@ import 'package:fstapp/app_router.gr.dart';
 import 'package:fstapp/router_service.dart';
 import 'package:fstapp/components/email_templates/email_template_model.dart';
 import 'package:fstapp/components/email_templates/db_email_templates.dart';
+import 'package:fstapp/components/email_templates/email_templates_strings.dart';
+import 'package:fstapp/services/dialog_helper.dart';
+import 'package:fstapp/components/_shared/common_strings.dart';
 
 import 'package:fstapp/components/html/html_editor_page.dart';
 import 'package:fstapp/services/toast_helper.dart';
-import 'package:fstapp/styles/styles_config.dart';
 import 'package:fstapp/theme_config.dart';
 import 'package:fstapp/components/html/html_view.dart';
+import 'package:fstapp/styles/styles_config.dart';
 
 class EmailTemplateSettingsPage extends StatefulWidget {
   final EmailTemplateModel template;
@@ -47,14 +50,14 @@ class _EmailTemplateSettingsPageState extends State<EmailTemplateSettingsPage> {
       widget.template.html = _htmlContent;
 
       // Set the integer fields from the emailTemplatesResponse if available.
-      widget.template.occasion = widget.emailTemplatesResponse.occasion.id;
+      widget.template.occasion = widget.emailTemplatesResponse.occasion?.id;
       widget.template.unit = widget.emailTemplatesResponse.unit.id;
       widget.template.organization =
           widget.emailTemplatesResponse.organization.id;
 
       await DbEmailTemplates.updateEmailTemplate(widget.template);
       ToastHelper.Show(
-          context, "${"Saved".tr()}: ${widget.template.subject ?? ''}");
+          context, "${CommonStrings.saved}: ${widget.template.subject ?? ''}");
       Navigator.of(context).pop();
     }
   }
@@ -101,178 +104,192 @@ class _EmailTemplateSettingsPageState extends State<EmailTemplateSettingsPage> {
   Widget build(BuildContext context) {
     // Get usage details from the email template (read-only info).
     final usageDetails = widget.template.getUsageDetails();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          usageDetails['title'] ?? '',
-          maxLines: 1,
-          style: TextStyle(
-            color: Theme.of(context).appBarTheme.titleTextStyle?.color ??
-                Colors.white,
+    return AlertDialog(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              usageDetails['title'] ?? '',
+              style: const TextStyle(fontSize: 20),
+            ),
           ),
-        ),
-        automaticallyImplyLeading: false,
-        actions: [
           IconButton(
             icon: const Icon(Icons.close),
             onPressed: () => Navigator.of(context).pop(),
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      contentPadding: const EdgeInsets.fromLTRB(24, 20, 24, 0),
+      actionsPadding: const EdgeInsets.fromLTRB(16, 0, 16, 12),
+      content: SizedBox(
+        width: StylesConfig.formMaxWidth, // Use max available width in dialog
         child: Form(
           key: _formKey,
-          child: Center(
-            child: ConstrainedBox(
-              constraints:
-              BoxConstraints(maxWidth: StylesConfig.formMaxWidth),
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-
-
-                  // Usage details container wrapped in a SelectionArea.
-                  SelectionArea(
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: ThemeConfig.grey150(context),
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: ThemeConfig.grey300(context)),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            usageDetails['title'] ?? '',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            usageDetails['description'] ?? '',
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: ThemeConfig.grey850(context),
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            "Available Substitutions:".tr(),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 4),
-                          _buildSubsDefinition(usageDetails['subs']),
-                        ],
-                      ),
-                    ),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              // Usage details container wrapped in a SelectionArea.
+              SelectionArea(
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: ThemeConfig.grey150(context),
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: ThemeConfig.grey300(context)),
                   ),
-                  const SizedBox(height: 16),
-                  // Subject text field.
-                  TextFormField(
-                    initialValue: _subject,
-                    decoration: InputDecoration(labelText: "Subject".tr()),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(
-                          errorText: "Subject is required".tr()),
-                    ]),
-                    onSaved: (val) => _subject = val,
-                    style: TextStyle(
-                      color: ThemeConfig.grey850(context),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  // Email Template Content Section header.
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: ThemeConfig.grey300(context),
-                          width: 1.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        usageDetails['title'] ?? '',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        usageDetails['description'] ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: ThemeConfig.grey850(context),
                         ),
                       ),
-                    ),
-                    child: Text(
-                      "Email Template Content".tr(),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Theme.of(context)
-                            .inputDecorationTheme
-                            .labelStyle
-                            ?.color ??
-                            ThemeConfig.grey700(context).withOpacity(0.85),
+                      const SizedBox(height: 8),
+                      Text(
+                        "Available Substitutions:".tr(),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  // Preview of the HTML content.
-                  HtmlView(
-                    html: _htmlContent ?? "",
-                    isSelectable: true,
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () async {
-                          final result = await RouterService.navigatePageInfo(
-                            context,
-                            HtmlEditorRoute(
-                              content: {HtmlEditorPage.parContent: _htmlContent},
-                              occasionId: widget.template.occasion
-                            ),
-                          );
-                          if (result != null && result is String) {
-                            setState(() {
-                              _htmlContent = result;
-                            });
-                          }
-                        },
-                        child: Text("Edit content".tr()),
-                      ),
+                      const SizedBox(height: 4),
+                      _buildSubsDefinition(usageDetails['subs']),
                     ],
                   ),
-                  const SizedBox(height: 16),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // Subject text field.
+              TextFormField(
+                initialValue: _subject,
+                decoration: InputDecoration(labelText: "Subject".tr()),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(
+                      errorText: "Subject is required".tr()),
+                ]),
+                onSaved: (val) => _subject = val,
+                style: TextStyle(
+                  color: ThemeConfig.grey850(context),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // Email Template Content Section header.
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: ThemeConfig.grey300(context),
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Email Template Content".tr(),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context)
+                        .inputDecorationTheme
+                        .labelStyle
+                        ?.color ??
+                        ThemeConfig.grey700(context).withOpacity(0.85),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              // Preview of the HTML content.
+              HtmlView(
+                html: _htmlContent ?? "",
+                isSelectable: true,
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      final result = await RouterService.navigatePageInfo(
+                        context,
+                        HtmlEditorRoute(
+                          content: {HtmlEditorPage.parContent: _htmlContent},
+                          occasionId: widget.template.occasion
+                        ),
+                      );
+                      if (result != null && result is String) {
+                        setState(() {
+                          _htmlContent = result;
+                        });
+                      }
+                    },
+                    child: Text("Edit content".tr()),
+                  ),
+                  if ((widget.template.occasion != null && widget.template.occasion == widget.emailTemplatesResponse.occasion?.id) ||
+                          (widget.template.unit != null && widget.template.unit == widget.emailTemplatesResponse.unit.id && widget.emailTemplatesResponse.occasion == null))
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: ThemeConfig.redColor(context),
+                              foregroundColor: Colors.white,
+                            ),
+                            onPressed: () async {
+                              final confirmed = await DialogHelper.showConfirmationDialog(
+                                context,
+                                EmailTemplatesStrings.resetConfirmTitle,
+                                EmailTemplatesStrings.resetConfirmContent,
+                                confirmButtonMessage: EmailTemplatesStrings.resetToDefault,
+                              );
+                              
+                              if (confirmed == true) {
+                                await DbEmailTemplates.deleteEntityEmailTemplate(
+                                  occasionId: widget.emailTemplatesResponse.occasion?.id,
+                                  unitId: widget.emailTemplatesResponse.occasion == null ? widget.emailTemplatesResponse.unit.id : null,
+                                  code: widget.template.code!,
+                                );
+
+                                if (context.mounted) {
+                                  Navigator.of(context).pop();
+                                }
+                              }
+                            },
+                            child: Text(EmailTemplatesStrings.resetToDefault),
+                          ),
+                        ),
                 ],
               ),
-            ),
+              const SizedBox(height: 16),
+            ],
           ),
         ),
       ),
-      bottomNavigationBar: Container(
-        padding:
-        const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-        color: Theme.of(context).appBarTheme.backgroundColor ??
-            Theme.of(context).primaryColor,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text("Storno".tr()),
-            ),
-            const SizedBox(width: 8),
-            ElevatedButton(
-              onPressed: _saveSettings,
-              child: Text("Save".tr()),
-            ),
-          ],
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text(CommonStrings.storno),
         ),
-      ),
+        ElevatedButton(
+          onPressed: _saveSettings,
+          child: Text(CommonStrings.save),
+        ),
+      ],
     );
   }
 }
