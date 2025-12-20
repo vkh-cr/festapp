25f16962883123c1e80cceba9b38e94861c3216a
miakh <2659269+miakh@users.noreply.github.com>
2025-12-20 22:42:38 +0100

    fx

--- scripts/functions/eshop_forms/get_form_by_link.sql
+++ scripts/functions/eshop_forms/get_form_by_link.sql
@@ -122,12 +122,6 @@ BEGIN
                 WHERE ff.form = f.id AND ff.is_hidden = false
             )
         )
-                            END
-                        )
-                    ) ORDER BY COALESCE(ff."order", 0)
-                )
-            )
-        )
     )
     INTO allData
     FROM public.forms f
