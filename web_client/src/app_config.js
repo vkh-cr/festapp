cda984e644b816f9e549afdd2549b7cf4baeb792
miakh <2659269+miakh@users.noreply.github.com>
2025-12-20 23:20:02 +0100

    adjust branch

--- web_client/src/app_config.js
+++ web_client/src/app_config.js
@@ -2,14 +2,14 @@ import { APP_VERSION } from './version.js';
 
 export class AppConfig {
     static version = APP_VERSION;
-    static supabaseUrl = 'https://kjdpmixlnhntmxjedpxh.supabase.co';
-    static anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZHBtaXhsbmhudG14amVkcHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NDI5NzEsImV4cCI6MjAxNzUxODk3MX0.06nTXCL-i1GxLckfEyCNlVVwt62QTzKUezqmsYSR_MI';
-    static organization = 1;
+    static supabaseUrl = 'https://lwfpdjxsdmkfyrzqbrlk.supabase.co';
+    static anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3ZnBkanhzZG1rZnlyenFicmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjgwNDksImV4cCI6MjAyNjgwNDA0OX0.440P3yf8ZqNjOHEQTn9uJhkKyObhD_7PZ2NwaE9e4J0';
+    static organization = 5;
     static forceOccasionLink = null;
     static isAllUnit = false;
     static isAppSupported = true;
     static flutterAppUrl = '';
-    static webLink = "https://live.festapp.net";
+    static webLink = "https://app.absolventskyvelehrad.cz";
     static compatibleUrls = [AppConfig.webLink];
 
     
