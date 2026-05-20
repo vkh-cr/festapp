3f6dd7375b78bfeaf5cca5a8d1ecb2ded1879f72
miakh <2659269+miakh@users.noreply.github.com>
2026-05-20 11:19:30 +0200

    csmostrava2026: set OneSignal app id (shared with AKSM Jubileum 2025)

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -14,7 +14,7 @@ class AppConfig {
   static const bool isOwnProgramSupported = true;
   static const bool isNotificationsSupported = true;
   static const bool isWebNotificationsSupported = true;
-  static const String oneSignalAppId = '';
+  static const String oneSignalAppId = '7140cfd2-280d-4971-a81a-b78503e99e6e';
   static const String defaultAdministrationRoute = AdminPage.ROUTE;
 
   static const int organization = 9;
