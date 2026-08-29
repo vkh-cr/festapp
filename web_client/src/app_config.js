9d3d1e99185f9b538722bec1173a13cef78509f5
Michael Bujnovský <2659269+miakh@users.noreply.github.com>
2026-08-29 14:25:47 +0200

    release: prepare Slunovrat transition web app [skip ci]

--- web_client/src/app_config.js
+++ web_client/src/app_config.js
@@ -2,39 +2,39 @@ import { APP_VERSION } from './version.js';
 
 export class AppConfig {
     static version = APP_VERSION;
-    static supabaseUrl = 'https://kjdpmixlnhntmxjedpxh.supabase.co';
-    static anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZHBtaXhsbmhudG14amVkcHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NDI5NzEsImV4cCI6MjAxNzUxODk3MX0.06nTXCL-i1GxLckfEyCNlVVwt62QTzKUezqmsYSR_MI';
-    static backendActivationTenantId = '';
-    static backendActivationManifestUrl = '';
-    static backendActivationCanonicalManifestSha256 = '';
-    static backendActivationCanonicalSupabaseUrl = '';
-    static backendActivationCanonicalAnonKey = '';
-    static backendActivationCanonicalOrganizationId = 0;
-    static backendActivationCanonicalProfileSha256 = '';
+    static supabaseUrl = 'https://hvtsoseaywurkmhywdbd.supabase.co';
+    static anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dHNvc2VheXd1cmttaHl3ZGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwMzk1MDYsImV4cCI6MjAzMTYxNTUwNn0.LIFBDLrSxTrI4z_Wwnt_5mS5SW2FC9ysVIhJ3m7FD10';
+    static backendActivationTenantId = 'festivalslunovrat';
+    static backendActivationManifestUrl = 'https://app.festivalslunovrat.cz/backend-activation.json';
+    static backendActivationCanonicalManifestSha256 = 'f8bb5614c9d40fb2255cdb3dac83da74bf327e41de47341f20ae1bd76fd7be26';
+    static backendActivationCanonicalSupabaseUrl = 'https://api.festapp.net';
+    static backendActivationCanonicalAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg3OTA4NjQyLCJleHAiOjE5NDU1ODg2NDJ9.uE20X1ueXZlul4F1fRfV5wXFVlGd8QKBFUHMIo1XD4M';
+    static backendActivationCanonicalOrganizationId = 19;
+    static backendActivationCanonicalProfileSha256 = 'e9384a8c6820b5a95122e4804c79e51c70e011da082430ea2a4c5472d99ad9cf';
     static organization = 1;
-    static forceOccasionLink = null;
+    static forceOccasionLink = "2025-copy-98cf835a";
     static isAllUnit = false;
     static isAppSupported = true;
-    static flutterAppUrl = '';
-    static webLink = "https://vstupenky.online";
-    static privacyUrl = "https://vstupenky.online/privacy/";
-    static privacyChoicesUrl = "https://vstupenky.online/privacy/choices/";
-    static termsUrl = "https://vstupenky.online/terms/";
-    static supportUrl = "https://vstupenky.online/support/";
-    static deleteAccountUrl = "https://vstupenky.online/delete-account/";
+    static flutterAppUrl = 'https://app.festivalslunovrat.cz';
+    static webLink = "https://app.festivalslunovrat.cz";
+    static privacyUrl = "https://app.festivalslunovrat.cz/privacy/";
+    static privacyChoicesUrl = "https://app.festivalslunovrat.cz/privacy/choices/";
+    static termsUrl = "https://app.festivalslunovrat.cz/terms/";
+    static supportUrl = "https://app.festivalslunovrat.cz/support/";
+    static deleteAccountUrl = "https://app.festivalslunovrat.cz/delete-account/";
     static imageApiUrl = 'https://image-api.festapp.net';
-    static imageProjectId = 'default';
+    static imageProjectId = 'slunovrat';
     static compatibleUrls = [AppConfig.webLink];
 
     
     // Localization
-    static supportedLanguages = ['cs'];
+    static supportedLanguages = ['cs', 'en', 'pl'];
     static defaultLanguage = 'cs';
 
     // Local Storage Keys
     static Keys = {
         theme: 'theme',
         locale: 'locale',
-        auth: 'sb-kjdpmixlnhntmxjedpxh-auth-token'
+        auth: 'sb-hvtsoseaywurkmhywdbd-auth-token'
     };
 }
